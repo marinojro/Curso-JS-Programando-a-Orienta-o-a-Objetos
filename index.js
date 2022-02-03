eb421c023e0edf39128cb6445d002c72cacef524
marinojro <82152115+marinojro@users.noreply.github.com>
2022-02-03 18:32:29 -0300

    alterando e acrecentando comentários

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
- 
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente{
+    agencia;
+    saldo;
 
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
+
+//Criando um novo Cliente
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
+//Criando uma nova Conta Corrente
+const contaCorrenteDoRicardo = new ContaCorrente();
+contaCorrenteDoRicardo.agencia = 1001;
+contaCorrenteDoRicardo.saldo = 0;
+
+//Operações de depósito e saque
+console.log(contaCorrenteDoRicardo.saldo)
+contaCorrenteDoRicardo.saldo = 100;
+console.log(contaCorrenteDoRicardo.saldo)
+contaCorrenteDoRicardo.sacar(50)
+console.log(contaCorrenteDoRicardo.saldo)
 
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
+const contaCorrenteDaAlice = new ContaCorrente();
+contaCorrenteDaAlice.agencia = 1001;
+contaCorrenteDaAlice.saldo = 0;
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 88822233309;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
-console.log(cliente1, cliente2);
+console.log(cliente1);
